2e40f1a89acaf3ca0d29b26f7b49cb2b2b2e3e91
manu <manumanuvk740@gmail.com>
2025-07-11 11:04:47 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,50 +1,58 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby,
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-	int i,j,k;
-	for(i=1; i<5; i++) {
-		for(j=1; j<=i; j++)
-		{
-			printf("*");
-		}
-		for(k=1; k<5-i; k++) {
+	int i,j,k,l;
+	for(i=1; i<8; i++) {
+		for(j=1; j<13-i; j++) {
 			printf(" ");
 		}
-		for(k=1; k<5-i; k++) {
-			printf(" ");
-		}
-		for(j=1; j<=i; j++)
-		{
-			printf("*");
-		}
+		for(k=1; k<=i; k++) {
 
-
-		printf("\n");
-	}
-
-	int l,m,n;
-	for(l=1; l<5; l++)
-	{
-		for(m=0; m<5-l; m++) {
-			printf("*");
-		}
-		for(n=2; n<l*2; n++) {
-			printf(" ");
-		}
-		for(m=0; m<5-l; m++) {
-			printf("*");
+			printf("* ");
 		}
 		printf("\n");
 	}
+	for(i=3; i<10; i++) {
+		for(j=1; j<13-i; j++) {
+			printf(" ");
+		}
+		for(k=1; k<=i; k++) {
 
-	return 0;
+			printf("* ");
+		    
+		}
+	    printf("\n");
+	}
+	for(i=5; i<12; i++) {
+		for(j=1; j<=12-i; j++) {
+			printf(" ");
+		}
+		for(k=1; k<=i; k++) {
+
+			printf("* ");
+		    
+		}
+	    printf("\n");
+	
+}
+for(i=1;i<3;i++){
+    	for(j=1; j<10; j++) {
+			printf(" ");
+    	}
+    for(l=1;l<3;l++){
+        printf("* ");
+    }
+    printf("\n");
+}
+
+return 0;
 }
